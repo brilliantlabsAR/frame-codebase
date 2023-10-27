9fc3390844a91eec4e86a9177ad41ce8e1d62f27
Raj Nakarja <raj@siliconwitchery.com>
2023-10-27 14:44:16 +0200

    Improved test script

--- tests/test_api.py
+++ tests/test_api.py
@@ -6,30 +6,56 @@ import asyncio
 from frameutils import Bluetooth
 
 
+class TestBluetooth(Bluetooth):
+    def __init__(self):
+        self._passed_tests = 0
+        self._failed_tests = 0
+
+    def _log_passed(self, lua_string, response):
+        self._passed_tests += 1
+        print(f"\033[92mPassed: {lua_string} => {response}")
+
+    def _log_failed(self, lua_string, response, expected):
+        self._failed_tests += 1
+        print(f"\033[91mFAILED: {lua_string} => {response} != {expected}")
+
+    async def initialize(self):
+        await self.connect(lua_response_handler=lambda str: None)
+
+    async def end(self):
+        passed_tests = self._passed_tests
+        total_tests = self._passed_tests + self._failed_tests
+        print("\033[0m")
+        print(f"Done! Passed {passed_tests} of {total_tests} tests")
+        await self.disconnect()
+
+    async def lua_equals(self, lua_string: str, expect: str):
+        response = await self.send_lua(f"print({lua_string})", wait=True)
+        if response == expect:
+            self._log_passed(lua_string, response)
+        else:
+            self._log_failed(lua_string, response, expect)
+
+    async def lua_has_length(self, lua_string: str, length: int):
+        response = await self.send_lua(f"print({lua_string})", wait=True)
+        if len(response) == length:
+            self._log_passed(lua_string, response)
+        else:
+            self._log_failed(lua_string, f"len({len(response)})", f"len({length})")
+
+
 async def main():
-    b = Bluetooth()
-    await b.connect(lua_response_handler=lambda str: None)
+    test = TestBluetooth()
+    await test.initialize()
 
-    print(await b.send_lua("print(device.NAME)", wait=True))
-    print(await b.send_lua("print(device.FIRMWARE_VERSION)", wait=True))
-    print(await b.send_lua("print(device.GIT_TAG)", wait=True))
-    print(await b.send_lua("print(device.mac_address())", wait=True))
-    print(await b.send_lua("print(device.battery_level())", wait=True))
+    # Test device module
+    await test.lua_equals("device.NAME", "frame")
+    await test.lua_has_length("device.FIRMWARE_VERSION", 12)
+    await test.lua_has_length("device.GIT_TAG", 7)
+    await test.lua_has_length("device.mac_address()", 17)
+    await test.lua_equals("device.battery_level()", "100.0")
 
-    print(await b.send_lua("print(device.stay_awake())", wait=True))
-
-    await b.send_lua("device.stay_awake(true)")
-    await asyncio.sleep(0.1)
-
-    print(await b.send_lua("print(device.stay_awake())", wait=True))
-
-    await b.send_lua("print(device.sleep())")
-    await asyncio.sleep(0.1)
-
-    await b.send_reset_signal()
-    await asyncio.sleep(0.1)
-
-    await b.disconnect()
+    await test.end()
 
 
 asyncio.run(main())
