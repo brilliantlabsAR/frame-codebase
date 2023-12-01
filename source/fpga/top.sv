ac5126947a8597d68b76254c2eb6104213605bec
Rajesh Nakarja <rajpaged@gmail.com>
2023-12-01 16:10:16 +0100

    Added PLL

--- source/fpga/top.sv
+++ source/fpga/top.sv
@@ -8,7 +8,7 @@
  *
  * Copyright © 2023 Brilliant Labs Limited
  */
-`ifdef YOSYS
+`ifndef RADIANT
 `include "modules/camera/camera.sv"
 `include "modules/graphics/display.sv"
 `include "modules/spi/spi_peripheral.sv"
@@ -42,14 +42,68 @@ module top (
 
 logic system_clock;
 
+// Clocking
 OSCA #(
-    .HF_CLK_DIV("8"), // 50 MHz
-    .HF_OSC_EN("ENABLED")
+    .HF_CLK_DIV("24"), // 18 MHz
+    .HF_OSC_EN("ENABLED"),
+    .LF_OUTPUT_EN("DISABLED")
     ) osc (
     .HFOUTEN(1'b1),
-    .HFCLKOUT(system_clock)
+    .HFCLKOUT(oscillator_clock)
 );
 
+`ifdef RADIANT
+
+logic camera_clock_24MHz;
+logic pixel_clock_36MHz;
+logic pixel_clock_72MHz;
+logic display_clock_50MHz;
+logic pll_locked;
+
+pll_ip pll_ip (
+    .clki_i(oscillator_clock),
+    .clkop_o(camera_clock_24MHz),
+    .clkos_o(pixel_clock_36MHz),
+    .clkos2_o(pixel_clock_72MHz),
+    .clkos3_o(display_clock_50MHz),
+    .clkos4_o(),
+    .clkos5_o(),
+    .lock_o(pll_locked)
+);
+
+logic global_reset_n;
+
+logic [7:0] reset_counter;
+always_ff @(posedge oscillator_clock) begin
+    if (pll_locked & !reset_counter[7]) begin
+        reset_counter <= reset_counter + 1;
+    end
+
+    else begin
+        reset_counter <= 0;
+    end
+end
+
+assign global_reset_n = pll_locked & reset_counter[7];
+
+logic reset_n_pixel_clock_72MHz;
+logic reset_n_display_clock_50MHz;
+
+reset_sync reset_sync_pixel_clock_72MHz (
+    .clock(pixel_clock_72MHz),
+    .async_reset_n(global_reset_n),
+    .sync_reset_n(reset_n_pixel_clock_72MHz)
+);
+
+reset_sync reset_sync_display_clock_50MHz (
+    .clock(display_clock_50MHz),
+    .async_reset_n(global_reset_n),
+    .sync_reset_n(reset_n_display_clock_50MHz)
+);
+
+`endif
+
+// SPI
 logic [7:0] subperipheral_address;
 logic subperipheral_address_valid;
 logic [7:0] subperipheral_copi;
@@ -66,7 +120,8 @@ logic [7:0] subperipheral_2_cipo;
 logic subperipheral_2_cipo_valid;
 
 spi_peripheral spi_peripheral (
-    .system_clock(system_clock),
+    .clock(pixel_clock_72MHz),
+	.reset_n(reset_n_pixel_clock_72MHz),
 
     .spi_select_in(spi_select_in),
     .spi_clock_in(spi_clock_in),
@@ -97,7 +152,8 @@ spi_subperipheral_selector spi_subperipheral_selector (
 );
 
 spi_register_chip_id spi_register_chip_id (
-    .system_clock(system_clock),
+    .clock(pixel_clock_72MHz),
+	.reset_n(reset_n_pixel_clock_72MHz),
     .enable(subperipheral_1_enable),
 
     .data_out(subperipheral_1_cipo),
@@ -105,7 +161,8 @@ spi_register_chip_id spi_register_chip_id (
 );
 
 spi_register_version_string spi_register_version_string (
-    .system_clock(system_clock),
+    .clock(pixel_clock_72MHz),
+	.reset_n(reset_n_pixel_clock_72MHz),
     .enable(subperipheral_2_enable),
     .data_in_valid(subperipheral_copi_valid),
 
@@ -113,6 +170,7 @@ spi_register_version_string spi_register_version_string (
     .data_out_valid(subperipheral_2_cipo_valid)
 );
 
+// Graphics
 // display display (
 //     .clock_in(clock),
 //     .clock_out(display_clock),
