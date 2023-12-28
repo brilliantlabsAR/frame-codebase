7e4fa0c5860b5b435c313482656dfb502f4860b7
Raj Nakarja <raj@siliconwitchery.com>
2023-12-28 13:51:28 +0000

    WIP frame buffer ram

--- source/fpga/modules/graphics/frame_buffers.sv
+++ source/fpga/modules/graphics/frame_buffers.sv
@@ -22,12 +22,165 @@ module frame_buffers (
 
     input logic switch_write_buffer_in
 );
-    
-reg [3:0] frame_buffer_a [0:160];
 
 logic currently_displayed_buffer = 0;
 logic [1:0] switch_write_buffer_edge_monitor = 0;
 logic buffer_switch_pending = 0;
+
+logic [13:0] frame_buffer_a_read_address;
+logic [13:0] frame_buffer_a_write_address;
+logic [7:0] frame_buffer_a_read_data;
+logic [7:0] frame_buffer_a_write_data;
+logic frame_buffer_a_write_enable;
+
+PDPSC512K #(
+    .OUTREG("NO_REG"),
+    .GSR("DISABLED"),
+    .RESETMODE("SYNC"),
+    .INITVAL_00(),
+    .INITVAL_01(),
+    .INITVAL_02(),
+    .INITVAL_03(),
+    .INITVAL_04(),
+    .INITVAL_05(),
+    .INITVAL_06(),
+    .INITVAL_07(),
+    .INITVAL_08(),
+    .INITVAL_09(),
+    .INITVAL_0A(),
+    .INITVAL_0B(),
+    .INITVAL_0C(),
+    .INITVAL_0D(),
+    .INITVAL_0E(),
+    .INITVAL_0F(),
+    .INITVAL_10(),
+    .INITVAL_11(),
+    .INITVAL_12(),
+    .INITVAL_13(),
+    .INITVAL_14(),
+    .INITVAL_15(),
+    .INITVAL_16(),
+    .INITVAL_17(),
+    .INITVAL_18(),
+    .INITVAL_19(),
+    .INITVAL_1A(),
+    .INITVAL_1B(),
+    .INITVAL_1C(),
+    .INITVAL_1D(),
+    .INITVAL_1E(),
+    .INITVAL_1F(),
+    .INITVAL_20(),
+    .INITVAL_21(),
+    .INITVAL_22(),
+    .INITVAL_23(),
+    .INITVAL_24(),
+    .INITVAL_25(),
+    .INITVAL_26(),
+    .INITVAL_27(),
+    .INITVAL_28(),
+    .INITVAL_29(),
+    .INITVAL_2A(),
+    .INITVAL_2B(),
+    .INITVAL_2C(),
+    .INITVAL_2D(),
+    .INITVAL_2E(),
+    .INITVAL_2F(),
+    .INITVAL_30(),
+    .INITVAL_31(),
+    .INITVAL_32(),
+    .INITVAL_33(),
+    .INITVAL_34(),
+    .INITVAL_35(),
+    .INITVAL_36(),
+    .INITVAL_37(),
+    .INITVAL_38(),
+    .INITVAL_39(),
+    .INITVAL_3A(),
+    .INITVAL_3B(),
+    .INITVAL_3C(),
+    .INITVAL_3D(),
+    .INITVAL_3E(),
+    .INITVAL_3F(),
+    .INITVAL_40(),
+    .INITVAL_41(),
+    .INITVAL_42(),
+    .INITVAL_43(),
+    .INITVAL_44(),
+    .INITVAL_45(),
+    .INITVAL_46(),
+    .INITVAL_47(),
+    .INITVAL_48(),
+    .INITVAL_49(),
+    .INITVAL_4A(),
+    .INITVAL_4B(),
+    .INITVAL_4C(),
+    .INITVAL_4D(),
+    .INITVAL_4E(),
+    .INITVAL_4F(),
+    .INITVAL_50(),
+    .INITVAL_51(),
+    .INITVAL_52(),
+    .INITVAL_53(),
+    .INITVAL_54(),
+    .INITVAL_55(),
+    .INITVAL_56(),
+    .INITVAL_57(),
+    .INITVAL_58(),
+    .INITVAL_59(),
+    .INITVAL_5A(),
+    .INITVAL_5B(),
+    .INITVAL_5C(),
+    .INITVAL_5D(),
+    .INITVAL_5E(),
+    .INITVAL_5F(),
+    .INITVAL_60(),
+    .INITVAL_61(),
+    .INITVAL_62(),
+    .INITVAL_63(),
+    .INITVAL_64(),
+    .INITVAL_65(),
+    .INITVAL_66(),
+    .INITVAL_67(),
+    .INITVAL_68(),
+    .INITVAL_69(),
+    .INITVAL_6A(),
+    .INITVAL_6B(),
+    .INITVAL_6C(),
+    .INITVAL_6D(),
+    .INITVAL_6E(),
+    .INITVAL_6F(),
+    .INITVAL_70(),
+    .INITVAL_71(),
+    .INITVAL_72(),
+    .INITVAL_73(),
+    .INITVAL_74(),
+    .INITVAL_75(),
+    .INITVAL_76(),
+    .INITVAL_77(),
+    .INITVAL_78(),
+    .INITVAL_79(),
+    .INITVAL_7A(),
+    .INITVAL_7B(),
+    .INITVAL_7C(),
+    .INITVAL_7D(),
+    .INITVAL_7E(),
+    .INITVAL_7F(),
+    .ASYNC_RESET_RELEASE("SYNC"),
+    .ECC_BYTE_SEL("BYTE_EN")
+) frame_buffer_a (
+    .DI(frame_buffer_a_write_data),
+    .ADW(frame_buffer_a_write_address),
+    .ADR(frame_buffer_a_read_address),
+    .CLK(clock_in),
+    .CEW(1),
+    .CER(1),
+    .WE(frame_buffer_a_write_enable),
+    .CSW(1),
+    .CSR(1),
+    .RSTR(0),
+    .BYTEEN_N('b0000),
+    .DO(frame_buffer_a_read_data)
+);
 
 always_ff @(posedge clock_in) begin
         
@@ -62,25 +215,17 @@ always_ff @(posedge clock_in) begin
         // 
         if (currently_displayed_buffer == 0) begin
     
-            pixel_read_data_out <= frame_buffer_a[pixel_read_address_in];
-
-            // // Odd pixel
-            // if (pixel_read_address_in[0]) begin
-            //     pixel_read_data_out <= frame_buffer_a[pixel_read_address_in >> 1][3:0];
-            //     // TODO write buffer B
-            // end
-
-            // // Even pixel
-            // else begin
-            //     pixel_read_data_out <= frame_buffer_a[pixel_read_address_in >> 1][7:4];
-            //     // TODO write buffer B
-            // end
+            frame_buffer_a_read_address <= pixel_read_address_in[13:0];
+            pixel_read_data_out <= frame_buffer_a_read_data[3:0];
+            frame_buffer_a_write_enable <= 0;
 
         end
 
         else begin
 
-            frame_buffer_a[pixel_write_address_in] <= pixel_write_data_in;
+            frame_buffer_a_write_address <= pixel_write_address_in[13:0];
+            frame_buffer_a_write_data <= pixel_write_data_in[3:0];
+            frame_buffer_a_write_enable <= 1;
 
             if      (pixel_read_address_in < 25  * 640) pixel_read_data_out <= 0;
             else if (pixel_read_address_in < 50  * 640) pixel_read_data_out <= 1;
@@ -99,24 +244,6 @@ always_ff @(posedge clock_in) begin
             else if (pixel_read_address_in < 375 * 640) pixel_read_data_out <= 14;
             else if (pixel_read_address_in < 400 * 640) pixel_read_data_out <= 15;
             else                                        pixel_read_data_out <= 0;
-
-            // Odd pixel
-            // if (pixel_read_address_in[0]) begin
-            //     frame_buffer_a[pixel_write_address_in >> 1] <= {
-            //             frame_buffer_a[pixel_write_address_in >> 1][7:4],
-            //             pixel_write_data_in
-            //         };
-            //     // pixel_read_data_out <= frame_buffer_b[pixel_read_address_in >> 1][3:0];
-            // end
-
-            // // Even pixel
-            // else begin
-            //     frame_buffer_a[pixel_write_address_in >> 1] <= {
-            //             pixel_write_data_in,
-            //             frame_buffer_a[pixel_write_address_in >> 1][3:0]
-            //         };
-            //     // pixel_read_data_out <= frame_buffer_b[pixel_read_address_in >> 1][7:4];
-            // end
 
         end
 
