c633a33d3e0dc6c5d8bb8486668306090efe4bdc
Raj Nakarja <raj@siliconwitchery.com>
2023-12-31 17:59:56 +0000

    Started wiring up sprite engine

--- source/fpga/modules/graphics/graphics.sv
+++ source/fpga/modules/graphics/graphics.sv
@@ -34,9 +34,22 @@ module graphics (
     output logic [2:0] display_cr_out
 );
 
+// TODO add buffers for metastability to inputs
+
+// Global drawing variables
+logic [9:0] cursor_x_position_reg; // 0 - 639
+logic [9:0] cursor_y_position_reg; // 0 - 399
+
+logic [9:0] sprite_draw_width_reg = 25; // 0 - 639
+logic [1:0] sprite_color_mode_reg = 16; // 0 = 1 color, 1 = 2 color
+                                   // 2 = 4 color, 3 = 16 color
+logic [3:0] sprite_pallet_offset_reg = 0; // 0 - 15
+
 // Registers to hold the current command operations
 logic clear_buffer_flag;
 logic assign_color_enable_flag;
+logic move_cursor_flag;
+logic sprite_draw_flag;
 logic show_buffer_flag;
 
 logic clear_buffer_in_progress_flag;
@@ -45,12 +58,19 @@ logic [17:0] clear_buffer_address_reg;
 logic [3:0] assign_color_index_reg;
 logic [9:0] assign_color_value_reg;
 
+logic [9:0] move_cursor_x_reg;
+logic [9:0] move_cursor_y_reg;
+
+logic [7:0] sprite_draw_data;
+
 // Handle op-codes as they come in
 always_ff @(posedge clock_in) begin
     
     if (op_code_valid_in == 0 || reset_n_in == 0) begin
         clear_buffer_flag <= 0;
         assign_color_enable_flag <= 0;
+        move_cursor_flag <= 0;
+        sprite_draw_flag <= 0;
         show_buffer_flag <= 0; 
     end
 
@@ -77,6 +97,48 @@ always_ff @(posedge clock_in) begin
                 end
             end
 
+            // Move cursor
+            'h12: begin
+                if (operand_valid_in) begin
+                    case (operand_count_in)
+                        1: move_cursor_x_reg <= {operand_in[1:0], 8'b0};
+                        2: move_cursor_x_reg <= {move_cursor_x_reg[1:0], operand_in};
+                        3: move_cursor_y_reg <= {operand_in[1:0], 8'b0};
+                        4: move_cursor_y_reg <= {move_cursor_x_reg[1:0], operand_in};
+                    endcase
+
+                    move_cursor_flag = operand_count_in == 4 ? 1 : 0;
+                end
+            end
+
+            // Set sprite draw width
+            'h13: begin
+
+            end
+
+            // Set sprite color mode
+            'h14: begin
+
+            end
+
+            // Set sprite color pallet offset
+            'h15: begin
+
+            end
+
+            // Sprite draw pixels
+            'h16: begin
+                if (operand_valid_in) begin
+                    sprite_draw_flag <= 1;
+                    sprite_draw_data <= operand_in;
+                end
+
+                else begin
+                    sprite_draw_flag <= 0;
+                end
+            end
+
+            // Show buffer
             'h19: begin
                 show_buffer_flag <= 1;
             end
@@ -87,8 +149,9 @@ always_ff @(posedge clock_in) begin
 
 end
 
-// Small statemachine to clear the screen
+// State machine to clear the screen
 always_ff @(posedge clock_in) begin
+    
     if (reset_n_in == 0) begin
         clear_buffer_in_progress_flag <= 0;
         clear_buffer_address_reg <= 0;
@@ -107,13 +170,39 @@ always_ff @(posedge clock_in) begin
                 clear_buffer_in_progress_flag <= 0;
             end
         end
+    end
 
+end
+
+// State machine to move and update the cursor from opcode, or draw operations
+always_ff @(posedge clock_in) begin
+
+    if (reset_n_in == 0) begin
+        cursor_x_position_reg <= 0;
+        cursor_y_position_reg <= 0;
+    end
+
+    else begin
+        if (move_cursor_flag) begin
+            cursor_x_position_reg <= move_cursor_x_reg;
+            cursor_y_position_reg <= move_cursor_y_reg;
+        end
+
+        else if (sprite_update_cursor_flag) begin
+            cursor_x_position_reg <= sprite_update_cursor_x_reg;
+            cursor_y_position_reg <= sprite_update_cursor_y_reg;
+        end
+
+        else if (vector_update_cursor_flag) begin
+            cursor_x_position_reg <= vector_update_cursor_x_reg;
+            cursor_y_position_reg <= vector_update_cursor_y_reg;
+        end
     end
 
 end
 
 // Feed display buffer based on active input
-logic pixel_write_enable_sprite_to_mux_wire = 0; // TODO clean this up
+logic pixel_write_enable_sprite_to_mux_wire;
 logic [17:0] pixel_write_address_sprite_to_mux_wire;
 logic [3:0] pixel_write_data_sprite_to_mux_wire;
 
@@ -196,5 +285,37 @@ display_driver display_driver (
     .display_cb_out(display_cb_out),
     .display_cr_out(display_cr_out)
 );
+
+// Sprite engine
+logic sprite_update_cursor_flag;
+logic [9:0] sprite_update_cursor_x_reg;
+logic [9:0] sprite_update_cursor_y_reg;
+
+sprite_engine sprite_engine (
+    .clock_in(clock_in),
+    .reset_n_in(reset_n_in),
+
+    .cursor_start_x_position_in(cursor_x_position_reg),
+    .cursor_start_y_position_in(cursor_y_position_reg),
+    .draw_width_in(sprite_draw_width_reg),
+    .color_mode_in(sprite_color_mode_reg),
+    .color_pallet_offset_in(sprite_pallet_offset_reg),
+
+    .sprite_draw_valid_in(sprite_draw_flag),
+    .sprite_draw_data_in(sprite_draw_data),
+
+    .pixel_write_enable_out(pixel_write_enable_sprite_to_mux_wire),
+    .pixel_write_address_out(pixel_write_address_sprite_to_mux_wire),
+    .pixel_write_data_out(pixel_write_data_sprite_to_mux_wire),
+
+    .cursor_end_position_valid_out(sprite_update_cursor_flag),
+    .cursor_end_x_position_out(sprite_update_cursor_x_reg),
+    .cursor_end_y_position_out(sprite_update_cursor_y_reg)
+);
+
+// Vector engine
+logic vector_update_cursor_flag;
+logic [9:0] vector_update_cursor_x_reg;
+logic [9:0] vector_update_cursor_y_reg;
 
 endmodule
--- source/fpga/modules/graphics/sprite_engine.sv
+++ source/fpga/modules/graphics/sprite_engine.sv
@@ -10,7 +10,34 @@
  */
 
  module sprite_engine (
+    input logic clock_in,
+    input logic reset_n_in,
 
+    input logic [9:0] cursor_start_x_position_in,
+    input logic [9:0] cursor_start_y_position_in,
+    input logic [9:0] draw_width_in,
+    input logic [1:0] color_mode_in,
+    input logic [3:0] color_pallet_offset_in,
+
+    input logic sprite_draw_valid_in,
+    input logic [7:0] sprite_draw_data_in,
+
+    output logic pixel_write_enable_out,
+    output logic [17:0] pixel_write_address_out,
+    output logic [3:0] pixel_write_data_out,
+
+    output logic cursor_end_position_valid_out,
+    output logic [9:0] cursor_end_x_position_out,
+    output logic [9:0] cursor_end_y_position_out
  );
+
+ initial begin
+    pixel_write_enable_out = 0;
+    pixel_write_address_out = 0;
+    pixel_write_data_out = 0;
+    cursor_end_position_valid_out = 0;
+    cursor_end_x_position_out = 0;
+    cursor_end_y_position_out = 0;
+ end
     
  endmodule
