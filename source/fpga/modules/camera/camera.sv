841463d38d851085a7caa3c4694d9b503fd9168d
Raj Nakarja <raj@siliconwitchery.com>
2024-01-31 15:25:33 +0100

    renamed variables in camera.sv deleted fifo.sv

--- source/fpga/modules/camera/camera.sv
+++ source/fpga/modules/camera/camera.sv
@@ -10,15 +10,11 @@
  */
 
 `ifndef RADIANT
-// `include "modules/camera/debayer.sv"
-// `include "modules/camera/fifo.sv"
+`include "modules/camera/debayer.sv"
 `include "modules/camera/image_buffer.sv"
 `endif
 
-module camera #(
-    CAPTURE_X_RESOLUTION = 200,
-    CAPTURE_Y_RESOLUTION = 200
-) (
+module camera (
     input logic global_reset_n_in,
     
     input logic clock_spi_in, // 72MHz
@@ -41,14 +37,12 @@ module camera #(
     output logic response_valid_out
 );
 
-// TODO add buffers for metastability to inputs?
-
 // Registers to hold the current command operations
 logic capture_flag;
 logic capture_in_progress_flag;
 
 // TODO make capture_size dynamic once we have adjustable resolution
-logic [15:0] capture_size = CAPTURE_X_RESOLUTION * CAPTURE_Y_RESOLUTION;
+logic [15:0] capture_size = 200 * 200;
 logic [15:0] bytes_read;
 
 logic [15:0] bytes_remaining;
@@ -128,24 +122,24 @@ always_ff @(posedge clock_spi_in) begin
 end
 
 // Capture command logic
-logic [1:0] frame_valid_cropped_edge_monitor;
-logic frame_valid_cropped;
+logic [1:0] cropped_frame_valid_edge_monitor;
+logic cropped_frame_valid;
 
 always_ff @(posedge clock_spi_in) begin
     if (reset_spi_n_in == 0) begin
         capture_in_progress_flag <= 0;
-        frame_valid_cropped_edge_monitor <= 0;
+        cropped_frame_valid_edge_monitor <= 0;
     end
 
     else begin
-        frame_valid_cropped_edge_monitor <= {frame_valid_cropped_edge_monitor[0],
-                                             frame_valid_cropped};
+        cropped_frame_valid_edge_monitor <= {cropped_frame_valid_edge_monitor[0],
+                                             cropped_frame_valid};
 
-        if (capture_flag && frame_valid_cropped_edge_monitor == 'b01) begin
+        if (capture_flag && cropped_frame_valid_edge_monitor == 'b01) begin
             capture_in_progress_flag <= 1;
         end
 
-        if (frame_valid_cropped_edge_monitor == 'b10) begin
+        if (cropped_frame_valid_edge_monitor == 'b10) begin
             capture_in_progress_flag <= 0;
         end
     end
@@ -153,107 +147,95 @@ end
 
 `ifdef RADIANT
 
-logic payload_en /* synthesis syn_keep=1 nomerge=""*/;
-logic sp_en /* synthesis syn_keep=1 nomerge=""*/;
-logic sp_en_d /* synthesis syn_keep=1 nomerge=""*/;
-logic lp_av_en /* synthesis syn_keep=1 nomerge=""*/;
-logic lp_av_en_d /* synthesis syn_keep=1 nomerge=""*/;
-logic [7:0] payload /* synthesis syn_keep=1 nomerge=""*/;
-logic [15:0] word_count /* synthesis syn_keep=1 nomerge=""*/;
-logic [5:0] datatype;
+logic mipi_byte_clock;
+logic mipi_byte_reset_n;
 
-logic clock_byte;
-logic reset_byte_n;
+logic mipi_payload_enable_metastable /* synthesis syn_keep=1 nomerge=""*/;
+logic mipi_payload_enable /* synthesis syn_keep=1 nomerge=""*/;
+
+logic [7:0] mipi_payload_metastable /* synthesis syn_keep=1 nomerge=""*/;
+logic [7:0] mipi_payload /* synthesis syn_keep=1 nomerge=""*/;
+
+logic mipi_sp_enable_metastable /* synthesis syn_keep=1 nomerge=""*/;
+logic mipi_sp_enable /* synthesis syn_keep=1 nomerge=""*/;
+
+logic mipi_lp_av_enable_metastable /* synthesis syn_keep=1 nomerge=""*/;
+logic mipi_lp_av_enable /* synthesis syn_keep=1 nomerge=""*/;
+
+logic [15:0] mipi_word_count /* synthesis syn_keep=1 nomerge=""*/;
+logic [5:0] mipi_datatype;
 
 reset_sync reset_sync_clock_byte (
-    .clock_in(clock_byte),
+    .clock_in(mipi_byte_clock),
     .async_reset_n_in(global_reset_n_in),
-    .sync_reset_n_out(reset_byte_n)
+    .sync_reset_n_out(mipi_byte_reset_n)
 );
 
 csi2_receiver_ip csi2_receiver_ip (
-    .clk_byte_o( ),
-    .clk_byte_hs_o(clock_byte),
-    .clk_byte_fr_i(clock_byte),
-    .reset_n_i(global_reset_n_in), // async reset
-    .reset_byte_fr_n_i(reset_byte_n),
+    .clk_byte_o(),
+    .clk_byte_hs_o(mipi_byte_clock),
+    .clk_byte_fr_i(mipi_byte_clock),
+    .reset_n_i(global_reset_n_in),
+    .reset_byte_fr_n_i(mipi_byte_reset_n),
     .clk_p_io(mipi_clock_p_in),
     .clk_n_io(mipi_clock_n_in),
     .d_p_io(mipi_data_p_in),
     .d_n_io(mipi_data_n_in),
-    .payload_en_o(payload_en),
-    .payload_o(payload),
+    .payload_en_o(mipi_payload_enable_metastable),
+    .payload_o(mipi_payload_metastable),
     .tx_rdy_i(1'b1),
     .pd_dphy_i(~global_reset_n_in),
-    .dt_o(datatype),
-    .wc_o(word_count),
-    .ref_dt_i(6'h2B), // RAW10 packet code
-    .sp_en_o(sp_en),
+    .dt_o(mipi_datatype),
+    .wc_o(mipi_word_count),
+    .ref_dt_i(6'h2B),
+    .sp_en_o(mipi_sp_enable_metastable),
     .lp_en_o(),
-    .lp_av_en_o(lp_av_en)
+    .lp_av_en_o(mipi_lp_av_enable_metastable)
 );
 
-always @(posedge clock_byte or negedge reset_byte_n) begin
-    if (!reset_byte_n) begin
-        lp_av_en_d <= 0;
-        sp_en_d <= 0;
+always @(posedge mipi_byte_clock or negedge mipi_byte_reset_n) begin
+
+    if (!mipi_byte_reset_n) begin
+        mipi_payload_enable <= 0;
+        mipi_payload <= 0;
+        mipi_sp_enable <= 0;
+        mipi_lp_av_enable <= 0;
     end
+
     else begin
-        lp_av_en_d <= lp_av_en;
-        sp_en_d <= sp_en;
+        mipi_payload_enable <= mipi_payload_enable_metastable;
+        mipi_payload <= mipi_payload_metastable;
+        mipi_sp_enable <= mipi_sp_enable_metastable;
+        mipi_lp_av_enable <= mipi_lp_av_enable_metastable;
     end
+
 end
 
-logic payload_en_1d, payload_en_2d, payload_en_3d /* synthesis syn_keep=1 nomerge=""*/;
-logic [7:0] payload_1d /* synthesis syn_keep=1 nomerge=""*/;
-logic [7:0] payload_2d /* synthesis syn_keep=1 nomerge=""*/;
-logic [7:0] payload_3d /* synthesis syn_keep=1 nomerge=""*/;
-always @(posedge clock_byte or negedge reset_byte_n) begin
-    if (!reset_byte_n) begin
-        payload_en_1d <= 0;
-        payload_en_2d <= 0;
-        payload_en_3d <= 0;
-
-        payload_1d <= 0;
-        payload_2d <= 0;
-        payload_3d <= 0;
-    end
-    else begin
-        payload_en_1d <= payload_en;
-        payload_en_2d <= payload_en_1d;
-        payload_en_3d <= payload_en_2d;
-
-        payload_1d <= payload;
-        payload_2d <= payload_1d;
-        payload_3d <= payload_2d;
-    end
-end
-
-logic frame_valid /* synthesis syn_keep=1 nomerge=""*/;
-logic line_valid /* synthesis syn_keep=1 nomerge=""*/;
-logic [9:0] pixel_data /* synthesis syn_keep=1 nomerge=""*/;
+logic byte_to_pixel_frame_valid /* synthesis syn_keep=1 nomerge=""*/;
+logic byte_to_pixel_line_valid /* synthesis syn_keep=1 nomerge=""*/;
+logic [9:0] byte_to_pixel_data /* synthesis syn_keep=1 nomerge=""*/;
 
 byte_to_pixel_ip byte_to_pixel_ip (
-    .reset_byte_n_i(reset_byte_n),
-    .clk_byte_i(clock_byte),
-    .sp_en_i(sp_en_d),
-    .dt_i(datatype),
-    .lp_av_en_i(lp_av_en_d),
-    .payload_en_i(payload_en_3d),
-    .payload_i(payload_3d),
-    .wc_i(word_count),
+    .reset_byte_n_i(mipi_byte_reset_n),
+    .clk_byte_i(mipi_byte_clock),
+    .sp_en_i(mipi_sp_enable),
+    .dt_i(mipi_datatype),
+    .lp_av_en_i(mipi_lp_av_enable),
+    .payload_en_i(mipi_payload_enable),
+    .payload_i(mipi_payload),
+    .wc_i(mipi_word_count),
     .reset_pixel_n_i(reset_pixel_n_in),
     .clk_pixel_i(clock_pixel_in),
-    .fv_o(frame_valid),
-    .lv_o(line_valid),
-    .pd_o(pixel_data)
+    .fv_o(byte_to_pixel_frame_valid),
+    .lv_o(byte_to_pixel_line_valid),
+    .pd_o(byte_to_pixel_data)
 );
 
-logic [11:0] pixel_red_data_debayer_to_crop;
-logic [11:0] pixel_green_data_debayer_to_crop;
-logic [11:0] pixel_blue_data_debayer_to_crop;
-logic line_valid_debayer_to_crop;
-logic frame_valid_debayer_to_crop;
+logic [11:0] debayered_red_data;
+logic [11:0] debayered_green_data;
+logic [11:0] debayered_blue_data;
+logic debayered_line_valid;
+logic debayered_frame_valid;
 
 debayer #(
     .X_RESOLUTION_IN(1288), // Include 1 left padding and 2 right padding
@@ -262,21 +244,21 @@ debayer #(
     .pixel_clock_in(clock_pixel_in),
     .reset_n_in(reset_pixel_n_in),
 
-    .pixel_data_in(pixel_data),
-    .line_valid_in(line_valid),
-    .frame_valid_in(frame_valid),
+    .pixel_data_in(byte_to_pixel_data),
+    .line_valid_in(byte_to_pixel_line_valid),
+    .frame_valid_in(byte_to_pixel_frame_valid),
 
-    .pixel_red_data_out(pixel_red_data_debayer_to_crop),
-    .pixel_green_data_out(pixel_green_data_debayer_to_crop),
-    .pixel_blue_data_out(pixel_blue_data_debayer_to_crop),
-    .line_valid_out(line_valid_debayer_to_crop),
-    .frame_valid_out(frame_valid_debayer_to_crop)
+    .pixel_red_data_out(debayered_red_data),
+    .pixel_green_data_out(debayered_green_data),
+    .pixel_blue_data_out(debayered_blue_data),
+    .line_valid_out(debayered_line_valid),
+    .frame_valid_out(debayered_frame_valid)
 );
 
-logic [9:0] pixel_red_data_cropped;
-logic [9:0] pixel_green_data_cropped;
-logic [9:0] pixel_blue_data_cropped;
-logic line_valid_cropped;
+logic [9:0] cropped_red_data;
+logic [9:0] cropped_green_data;
+logic [9:0] cropped_blue_data;
+logic cropped_line_valid;
 
 crop #(
     .X_CROP_START(544),
@@ -287,49 +269,42 @@ crop #(
     .pixel_clock_in(clock_pixel_in),
     .reset_n_in(reset_pixel_n_in),
 
-    .pixel_red_data_in(pixel_red_data_debayer_to_crop[11:2]),
-    .pixel_green_data_in(pixel_green_data_debayer_to_crop[11:2]),
-    .pixel_blue_data_in(pixel_blue_data_debayer_to_crop[11:2]),
-    .line_valid_in(line_valid_debayer_to_crop),
-    .frame_valid_in(frame_valid_debayer_to_crop),
+    .pixel_red_data_in(debayered_red_data[11:2]),
+    .pixel_green_data_in(debayered_green_data[11:2]),
+    .pixel_blue_data_in(debayered_blue_data[11:2]),
+    .line_valid_in(debayered_line_valid),
+    .frame_valid_in(debayered_frame_valid),
 
-    .pixel_red_data_out(pixel_red_data_cropped),
-    .pixel_green_data_out(pixel_green_data_cropped),
-    .pixel_blue_data_out(pixel_blue_data_cropped),
-    .line_valid_out(line_valid_cropped),
-    .frame_valid_out(frame_valid_cropped)
+    .pixel_red_data_out(cropped_red_data),
+    .pixel_green_data_out(cropped_green_data),
+    .pixel_blue_data_out(cropped_blue_data),
+    .line_valid_out(cropped_line_valid),
+    .frame_valid_out(cropped_frame_valid)
 );
 
-logic buffer_write_enable /* synthesis syn_keep=1 nomerge=""*/;
 logic [15:0] buffer_write_address;
-logic [15:0] buffer_write_data;
-assign buffer_write_data = {
-        pixel_red_data_cropped[9:7],
-        pixel_green_data_cropped[9:7],
-        pixel_blue_data_cropped[9:8]};
 
 always_ff @(posedge clock_pixel_in) begin
-    if (frame_valid_cropped == 0) begin
+
+    if (cropped_frame_valid == 0) begin
         buffer_write_address <= 0;
-        buffer_write_enable <= 0;
     end
-    else if (frame_valid_cropped & line_valid_cropped & capture_in_progress_flag) begin
+    else if (cropped_frame_valid & cropped_line_valid) begin
         buffer_write_address <= buffer_write_address + 1;
-        buffer_write_enable <= 1;
     end
-    else begin
-        buffer_write_enable <= 0;
-    end
+
 end
+
+// TODO add clock crossing from cropper to imagebuffer
 
 image_buffer image_buffer (
     .clock(clock_spi_in),
     .reset_n(reset_spi_n_in),
     .write_address(buffer_write_address),
     .read_address(buffer_read_address),
-    .write_data(buffer_write_data),
+    .write_data({cropped_red_data[9:7], cropped_green_data[9:7], cropped_blue_data[9:8]}),
     .read_data(buffer_read_data),
-    .write_enable(buffer_write_enable)
+    .write_enable(cropped_frame_valid & cropped_line_valid & capture_in_progress_flag)
 );
 
 `endif
