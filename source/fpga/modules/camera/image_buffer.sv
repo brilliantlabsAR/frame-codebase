d4a9778c775af4d1102f3e9c41a7f7edaed1e680
Raj Nakarja <rajpaged@gmail.com>
2024-02-02 10:24:57 +0000

    lram primitive for image buffer

--- source/fpga/modules/camera/image_buffer.sv
+++ source/fpga/modules/camera/image_buffer.sv
@@ -22,7 +22,32 @@ module image_buffer (
     input logic write_enable_in
 );
 
-`ifndef RADIANT (* ram_style="huge" *) `endif logic [31:0] mem [0:16384];
+logic [13:0] ram_address;
+logic [31:0] ram_write_data;
+logic [31:0] ram_read_data;
+logic ram_write_enable;
+
+// `ifndef RADIANT (* ram_style="huge" *) `endif logic [31:0] mem [0:16383];
+PDPSC512K #(
+    .OUTREG("NO_REG"),
+    .GSR("DISABLED"),
+    .RESETMODE("SYNC"),
+    .ASYNC_RESET_RELEASE("SYNC"),
+    .ECC_BYTE_SEL("BYTE_EN")
+) image_mem (
+    .DI(ram_write_data),
+    .ADW(ram_address),
+    .ADR(ram_address),
+    .CLK(clock_in),
+    .CEW(1'b1),
+    .CER(1'b1),
+    .WE(ram_write_enable),
+    .CSW(1'b1),
+    .CSR(1'b1),
+    .RSTR(~reset_n_in),
+    .BYTEEN_N(4'b000),
+    .DO(ram_read_data)
+);
 
 always @(posedge clock_in) begin
 
@@ -33,24 +58,31 @@ always @(posedge clock_in) begin
     end
 
     else begin
-        
+
         if (write_enable_in) begin
+            ram_write_enable <= 1;
+            ram_address <= write_address_in[15:2];
 
             case (write_address_in[1:0])
-                'd0: mem[write_address_in[15:2]] <= {mem[write_address_in[15:2]][31:8],  write_data_in                                   };
-                'd1: mem[write_address_in[15:2]] <= {mem[write_address_in[15:2]][31:16], write_data_in, mem[write_address_in[15:2]][7:0] };
-                'd2: mem[write_address_in[15:2]] <= {mem[write_address_in[15:2]][31:24], write_data_in, mem[write_address_in[15:2]][15:0]};
-                'd3: mem[write_address_in[15:2]] <= {                                    write_data_in, mem[write_address_in[15:2]][23:0]};
+                'd0: ram_write_data <= {ram_read_data[31:8],  write_data_in                                   };
+                'd1: ram_write_data <= {ram_read_data[31:16], write_data_in, ram_read_data[7:0] };
+                'd2: ram_write_data <= {ram_read_data[31:24], write_data_in, ram_read_data[15:0]};
+                'd3: ram_write_data <= {                                    write_data_in, ram_read_data[23:0]};
             endcase
 
         end
 
-        case (read_address_in[1:0])
-            'd0: read_data_out <= mem[read_address_in[15:2]][7:0];
-            'd1: read_data_out <= mem[read_address_in[15:2]][15:8];
-            'd2: read_data_out <= mem[read_address_in[15:2]][23:16];
-            'd3: read_data_out <= mem[read_address_in[15:2]][31:24];
-        endcase
+        else begin
+            ram_write_enable <= 0;
+            ram_address <= read_address_in[15:2];
+
+            case (read_address_in[1:0])
+                'd0: read_data_out <= ram_read_data[7:0];
+                'd1: read_data_out <= ram_read_data[15:8];
+                'd2: read_data_out <= ram_read_data[23:16];
+                'd3: read_data_out <= ram_read_data[31:24];
+            endcase
+        end
 
     end
 
