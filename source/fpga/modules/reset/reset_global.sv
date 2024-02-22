18ce2348d6fbf9848754c5a4b905ce37390fbc32
rohit <rohitrathnam>
2024-02-22 18:15:29 +0100

    update byte2pixel constraints, global resets

--- source/fpga/modules/reset/reset_global.sv
+++ source/fpga/modules/reset/reset_global.sv
@@ -12,23 +12,37 @@
 module reset_global (
 	input logic clock_in,
 	input logic pll_locked_in,
+    output logic pll_reset_out,
 	output logic global_reset_n_out
 );
 
-    logic [7:0] reset_counter /* synthesis syn_keep=1 nomerge=""*/;
+    logic [7:0] global_reset_counter /* synthesis syn_keep=1 nomerge=""*/;
+    logic [7:0] pll_reset_counter = 0 /* synthesis syn_keep=1 nomerge=""*/;
 
     always_ff @(posedge clock_in) begin
 
-        if (pll_locked_in & !reset_counter[7]) begin
-            reset_counter <= reset_counter + 1;
+        if (!pll_reset_counter[7]) begin
+            pll_reset_counter <= pll_reset_counter + 1;
+            global_reset_n_out <= 0;
+		    global_reset_counter <= 0;
+            pll_reset_out <= 1;
         end
 
-        if (!pll_locked_in) begin
-            reset_counter <= 0;
+        else begin
+
+            if (pll_locked_in & !global_reset_counter[7]) begin
+                global_reset_counter <= global_reset_counter + 1;
+            end
+
+            if (!pll_locked_in) begin
+                global_reset_counter <= 0;
+            end
+
+            global_reset_n_out <= pll_locked_in && global_reset_counter[7];
+            pll_reset_out <= 0;
+
         end
-        
+
     end
-
-    assign global_reset_n_out = pll_locked_in && reset_counter[7];
 
 endmodule
