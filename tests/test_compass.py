a4a00393c0b6c401560b2cc6ddbc9cbff92c0c9c
Raj Nakarja <raj@siliconwitchery.com>
2024-02-22 17:40:05 +0100

    Added imu.raw() function

---
+++ tests/test_compass.py
@@ -0,0 +1,27 @@
+"""
+Tests the Frame specific Lua libraries over Bluetooth.
+"""
+
+import asyncio
+from frameutils import Bluetooth
+
+
+async def main():
+    b = Bluetooth()
+
+    await b.connect()
+
+    while True:
+        await b.send_lua("resp = frame.imu.raw()")
+        print(
+            await b.send_lua(
+                "print(tostring(resp['compass']['x'])..'\t'..tostring(resp['compass']['y'])..'\t'..tostring(resp['compass']['z']))",
+                await_print=True,
+            )
+        )
+        asyncio.sleep(0.1)
+
+    await b.disconnect()
+
+
+asyncio.run(main())
