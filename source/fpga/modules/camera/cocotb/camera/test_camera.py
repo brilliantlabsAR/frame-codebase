87f9ac74fc4479058baf431b202cbecfea54a20a
Robert M <RMetchev@hotmail.com>
2024-03-05 11:53:50 -0800

    get it to work with modelsim

--- source/fpga/modules/camera/cocotb/camera/test_camera.py
+++ source/fpga/modules/camera/cocotb/camera/test_camera.py
@@ -4,7 +4,9 @@ from cocotb.triggers import ClockCycles, RisingEdge, FallingEdge, Timer
 import sys, os, time
 
 import numpy as np
-import cv2
+
+if os.environ['SIM'] != 'modelsim':
+    import cv2
 
 from encoder import writeJPG_header, writeJPG_footer    # ../jed
 
@@ -32,10 +34,11 @@ async def clock_n_reset(c, r, f, n=5):
  
 
 async def show_image(*img_files, t=5000):
-    for img_file in img_files:
-        cv2.imshow(img_file, cv2.imread(img_file))
-    cv2.waitKey(t) 
-    cv2.destroyAllWindows()
+    if os.environ['SIM'] != 'modelsim':
+        for img_file in img_files:
+            cv2.imshow(img_file, cv2.imread(img_file))
+        cv2.waitKey(t) 
+        cv2.destroyAllWindows()
 
 
 class SPITransactor():
@@ -80,21 +83,30 @@ class SPITransactor():
 
 
 class Tester(SPITransactor):
-    def __init__(self, dut, img_file='baboon.bmp'):
+    def __init__(self, dut, img_file='baboon.bmp', read_bmp=True, save_bmp_to_array=False):
         super().__init__(dut)
         self.dut = dut
         self.jpeg_sel = 0
         
-        # Always Read RGB image or bayer
-        self.img_bgr = cv2.imread(img_file)
+        # Always Read RGB image
+        if read_bmp and os.environ['SIM'] != 'modelsim':
+            self.img_bgr = cv2.imread(img_file)
+            if save_bmp_to_array:
+                with open(img_file + '.npy', 'wb') as f:
+                    np.save(f, self.img_bgr)
+        else:
+            with open(img_file + '.npy', 'rb') as f:
+                self.img_bgr = np.load(f)
 
-        self.img_bgr = np.vstack((self.img_bgr, self.img_bgr))
-        self.img_bgr = np.hstack((self.img_bgr, self.img_bgr))
-
+        # Makse sure at least 720x720
+        self.img_bgr = np.vstack([self.img_bgr] * np.ceil(720/np.shape(self.img_bgr)[0]).astype(int))
+        self.img_bgr = np.hstack([self.img_bgr] * np.ceil(720/np.shape(self.img_bgr)[1]).astype(int))
+        
         self.y, self.x, _ = np.shape(self.img_bgr)
         assert self.y%2 == 0
         assert self.x%2 == 0
 
+        # make bayer
         self.img_bayer = np.empty((self.y, self.x), dtype=np.uint8)        
         self.img_bayer[0::2, 0::2] = 0 + self.img_bgr[0::2, 0::2, 0] # top left B
         self.img_bayer[0::2, 1::2] = 0 + self.img_bgr[0::2, 1::2, 1] # top right G
@@ -151,6 +163,7 @@ class Tester(SPITransactor):
             
             await ClockCycles(self.dut.clock_pixel_in, blank)
         self.dut.byte_to_pixel_frame_valid.value = 0
+        await ClockCycles(self.dut.clock_pixel_in, blank)
 
 
     async def read_rgb_buffer(self):
@@ -227,7 +240,8 @@ class Tester(SPITransactor):
     async def write_bmp(self, filename='rgb_out.bmp'):
         #cv2.imwrite(img_file + '.orig.bmp', self.img_bgr)
         #import imageio; imageio.imwrite('file_name.jpg', self.img_bgr[:,:,[2,1,0]])
-        cv2.imwrite(filename, self.bgr_out)    
+        if os.environ['SIM'] != 'modelsim':
+            cv2.imwrite(filename, self.bgr_out)    
 
 
     async def write_image(self):
@@ -254,15 +268,23 @@ async def dct_test(dut):
     #test_image = '4.2.03.tiff'  # baboon 512x512
     
     test_image = '../jenc/' + test_image;
-    t = Tester(dut, test_image)
+    t = Tester(dut, test_image, read_bmp=False)
 
-    await t.initialize_encoder()    
-    bayer  = cocotb.start_soon(t.send_bayer())   
+    for _ in range(1):
+        if False:
+            # send non capture frame
+            bayer  = cocotb.start_soon(t.send_bayer())   
+            await cocotb.triggers.Combine(bayer)  # wait for frame end
 
-    await t.read_image_buffer()
-    await t.write_image()
+
+        # send capture frame
+        await t.initialize_encoder()    
+        bayer  = cocotb.start_soon(t.send_bayer())   
+
+        await t.read_image_buffer()
+        await t.write_image()
     
-    await show_image(test_image, 'jpeg_out.jpg' if t.jpeg_sel else 'rgb_out.bmp', t=0)
+        await show_image(test_image, 'jpeg_out.jpg' if t.jpeg_sel else 'rgb_out.bmp', t=0)
 
-    await cocotb.triggers.Combine(bayer)  # wait for frame end
-    await ClockCycles(dut.clock_spi_in, 100)  # wait for falling edge/"negedge"
+        await cocotb.triggers.Combine(bayer)  # wait for frame end
+    await ClockCycles(dut.clock_spi_in, 100)
