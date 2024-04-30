ada4fafcba9537e6cd163f11922e18f714fce078
rohit <rohitrathnam>
2024-04-30 08:54:01 +0200

    WIP histogram timing ok, image broken

--- source/fpga/modules/camera/histogram.sv
+++ source/fpga/modules/camera/histogram.sv
@@ -27,17 +27,37 @@ module histogram #(
     output logic histogram_ready_out
 );
 
-localparam BIN_SIZE = 1024 / NUM_BINS;
+localparam BIN_SIZE = 256 / NUM_BINS;
 
 logic previous_frame_valid;
+logic previous_line_valid;
 
-logic [15:0] red_histogram [0:NUM_BINS-1];
-logic [15:0] green_histogram [0:NUM_BINS-1];
-logic [15:0] blue_histogram [0:NUM_BINS-1];
+logic [7:0] red_histogram [0:NUM_BINS-1];
+logic [7:0] green_histogram [0:NUM_BINS-1];
+logic [7:0] blue_histogram [0:NUM_BINS-1];
+
+logic [7:0] red_data;
+logic [7:0] green_data;
+logic [7:0] blue_data;
+
+always_comb begin
+    red_data = red_data_in[9:2];
+    green_data = green_data_in[9:2];
+    blue_data = blue_data_in[9:2];
+end
+
+logic [3:0] red_index;
+logic [3:0] green_index;
+logic [3:0] blue_index;
+
+logic [7:0] red_previous_value;
+logic [7:0] green_previous_value;
+logic [7:0] blue_previous_value;
 
 always_ff @(posedge clock_in) begin
 
     previous_frame_valid <= frame_valid_in;
+    previous_line_valid <= line_valid_in;
 
     if (frame_valid_in == 0 || reset_n_in == 0) begin
 
@@ -45,14 +65,18 @@ always_ff @(posedge clock_in) begin
             histogram_ready_out <= 0;
 
             for (integer i=0; i<NUM_BINS; i++) begin
-                red_histogram_out[i] <= red_histogram[i][15:8];
-                green_histogram_out[i] <= green_histogram[i][15:8];
-                blue_histogram_out[i] <= blue_histogram[i][15:8];
+                red_histogram_out[i] <= red_histogram[i];
+                green_histogram_out[i] <= green_histogram[i];
+                blue_histogram_out[i] <= blue_histogram[i];
 
                 red_histogram[i] <= 0;
                 green_histogram[i] <= 0;
                 blue_histogram[i] <= 0;
             end
+
+            red_index <= 0;
+            green_index <= 0;
+            blue_index <= 0;
         end
 
         else begin
@@ -63,24 +87,39 @@ always_ff @(posedge clock_in) begin
     end
 
     else begin
+        histogram_ready_out <= 0;
 
         if (frame_valid_in && line_valid_in) begin
 
-            histogram_ready_out <= 0;
-            
-            // Increment corresponding bin count
-
+            // Increment corresponding bin
             for (integer i=0; i<NUM_BINS; i++) begin
-                if (red_data_in >= BIN_SIZE*i && red_data_in <= BIN_SIZE*(i+1)) begin
-                    red_histogram[i] <= red_histogram[i] + 1;
+                if (red_data >= BIN_SIZE*i && red_data <= BIN_SIZE*(i+1)) begin
+                    red_index <= i;
+                    red_previous_value <= red_histogram[i];
                 end
-                if (green_data_in >= BIN_SIZE*i && green_data_in <= BIN_SIZE*(i+1)) begin
-                    green_histogram[i] <= green_histogram[i] + 1;
+                if (green_data >= BIN_SIZE*i && green_data <= BIN_SIZE*(i+1)) begin
+                    green_index <= i;
+                    green_previous_value <= green_histogram[i];
                 end
-                if (blue_data_in >= BIN_SIZE*i && blue_data_in <= BIN_SIZE*(i+1)) begin
-                    blue_histogram[i] <= blue_histogram[i] + 1;
+                if (blue_data >= BIN_SIZE*i && blue_data <= BIN_SIZE*(i+1)) begin
+                    blue_index <= i;
+                    blue_previous_value <= blue_histogram[i];
                 end
             end
+
+        end
+
+        if (frame_valid_in && previous_line_valid) begin
+            
+            if (red_previous_value < 'hff)
+                red_histogram[red_index] <= red_previous_value + 1;
+            
+            if (green_previous_value < 'hff)
+                green_histogram[green_index] <= green_previous_value + 1;
+
+            if (blue_previous_value < 'hff)
+                blue_histogram[blue_index] <= blue_previous_value + 1;
+
         end
 		
     end
