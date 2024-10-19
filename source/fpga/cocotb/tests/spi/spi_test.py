9511f2ac3b5dc1c9921c922d3282e918fda2aa35
Robert M <RMetchev@hotmail.com>
2024-10-19 07:19:03 +0200

    update notes

--- source/fpga/cocotb/tests/spi/spi_test.py
+++ source/fpga/cocotb/tests/spi/spi_test.py
@@ -39,39 +39,38 @@ async def spi_test(dut):
     
     #await Timer(20, units='us')
     
-    # 1. Test single byte read from ID register 0xDB
+    #   1. Test single byte read from ID register 0xDB
     a = 0xdb
     id = [0x81]
     read_bytes = await t.spi_read(0xdb)
     assert read_bytes == id , f"ID register {hex(a)}: Expected: {[hex(i) for i in id]}. Received: {[hex(i) for i in read_bytes]}."
     
-    # 2. Now lets power up the PLL 0x40/0x41
+    #   2. Now lets power up the PLL 0x40/0x41
     await t.spi_write(0x40, 0x1)
-    # Wait for PLL to lock & global reset to kick in - 20ms
+    #   Wait for PLL to lock & global reset to kick in - 20ms
     await Timer(20, units='us')
-    # Check PLL lock flag
+    #   Check PLL lock flag
     a = 0x41
     read_bytes = await t.spi_read(a)
     assert read_bytes == [1] , f"ID register {hex(a)}: Expected: 0x1. Received: {[hex(i) for i in read_bytes]}."
 
-    # 3. Image Buffer clock switch
-
-    # 3a. Read Image buffer using PLL clock (default)
+    #   3. Test image buffer clock switch
+    #   3a. Read image buffer using PLL clock (default)
     read_bytes = await t.spi_read(0x22)
-    # 3b. Switch Image buffer clock to SPI clock 0x41
+    #   3b. Switch image buffer clock to SPI clock 0x41
     await t.spi_write(0x40, 0x3)
-    # 3c. Power down PLL 0x41
+    #   3c. Power down PLL 0x41
     await t.spi_write(0x40, 0x2)
-    # 3d. Read 2 bytes from Image buffer using SPI clock - low power PLL mode
+    #   3d. Read 2 bytes from Image buffer using SPI clock - low power PLL mode
     read_bytes = await t.spi_read(0x22, 2)
-    # 3e. Power up PLL again
+    #   3e. Power up PLL again
     await t.spi_write(0x40, 0x3)
     await Timer(20, units='us')
     read_bytes = await t.spi_read(0x41)
     assert read_bytes == [1]
-    # 3f. Switch Image buffer clock back to PLL clock
+    #   3f. Switch Image buffer clock back to PLL clock
     await t.spi_write(0x40, 0x1)
-    # 3d. Read 3 bytes from Image buffer using PLL clock
+    #   3d. Read 3 bytes from Image buffer using PLL clock
     read_bytes = await t.spi_read(0x22, 3)
 
 
