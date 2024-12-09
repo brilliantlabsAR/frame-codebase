6b37f818de61978627c7a677298b217426184021
Robert M <RMetchev@hotmail.com>
2024-12-10 00:15:05 +0100

    add graphics test

--- source/fpga/cocotb/tests/graphics/graphics_test.py
+++ source/fpga/cocotb/tests/graphics/graphics_test.py
@@ -51,28 +51,47 @@ async def spi_test(dut):
     read_bytes = await t.spi_read(a)
     assert read_bytes == [1] , f"ID register {hex(a)}: Expected: 0x1. Received: {[hex(i) for i in read_bytes]}."
 
-    #   3. Test image buffer clock switch
-    #   3a. Read image buffer using PLL clock (default)
-    read_bytes = await t.spi_read(0x22, 18)
-    #   3b. Switch image buffer clock to SPI clock 0x40
-    await t.spi_write(0x40, 0x3)
-    #   3c. Power down PLL 0x40
-    await t.spi_write(0x40, 0x2)
-    #   3d. Read 8 bytes from Image buffer using SPI clock - low power PLL mode
-    read_bytes = await t.spi_read(0x22, 18)
-    #   3e. Power up PLL again
-    await t.spi_write(0x40, 0x3)
-    await Timer(20, units='us')
-    read_bytes = await t.spi_read(0x41)
-    assert read_bytes == [1]
-    #   3f. Switch Image buffer clock back to PLL clock
-    await t.spi_write(0x40, 0x1)
-    #   3d. Read 8 bytes from Image buffer using PLL clock
-    read_bytes = await t.spi_read(0x22, 18)
+    #   4. Test Graphics
+    # Switch/clear command
+    await Timer(15, units='us')
+    await t.spi_write(0x14, 0)
 
-    if 1:
-        await Timer(15, units='us')
-        raise cocotb.result.TestSuccess("Test passed early")
+    #           // Draw pixels
+    #           send_opcode('h12);
+    #           send_operand('h00); // X pos
+    #           send_operand('h32);
+    #           send_operand('h00); // Y pos
+    #           send_operand('h64);
+    #           send_operand('h00); // Width
+    #           send_operand('h14);
+    #           send_operand('h10); // Total colors
+    #           send_operand('h00); // palette offset
+    #           send_operand('h12); // Data
+    #           send_operand('h34);
+    #           send_operand('h56);
+    #           send_operand('h78);
+    #           send_operand('h9A);
+    #           send_operand('hBC);
+    #           send_operand('hDE);
+    #           send_operand('hF0);
+    #           done();
+    #           #30000
+    await Timer(10, units='ms')
+    await t.spi_write(0x12, [0x00, 0x32, 0x00, 0x64, 0x00, 0x14, 0x10, 0x00, 0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC, 0xDE, 0xF0])
+    await t.spi_write(0x14, 0)
+
+    #           // Show command
+    #           send_opcode('h14);
+    #           done();
+    #           #5000000
+    await Timer(10, units='ms')
+    await t.spi_write(0x14, 0)
+    await Timer(20, units='ms')
+    await t.spi_write(0x14, 0)
+    await Timer(20, units='ms')
+    await t.spi_write(0x14, 0)
+    await Timer(20, units='ms')
+    await t.spi_write(0x14, 0)
 
     # Finish
-    await Timer(10, units='us')
+    await Timer(10, units='ms')
