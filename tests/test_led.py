7069aa94969d99445cecb279aec0204c7b2a5f65
Raj Nakarja <raj@siliconwitchery.com>
2024-12-13 09:49:07 +0100

    Added LED and display animation tests

--- tests/test_led.py
+++ tests/test_led.py
@@ -11,10 +11,10 @@ async def main():
 
     await b.connect(print_response_handler=lambda s: print(s))
 
-    # await b.send_lua("frame.led.set_color(100, 0, 0)")
+    await b.send_lua("frame.led.set_color(100, 0, 0)")
     await asyncio.sleep(1)
 
-    # await b.send_lua("frame.led.set_color(0, 100, 0)")
+    await b.send_lua("frame.led.set_color(0, 100, 0)")
     await asyncio.sleep(1)
 
     await b.send_lua("frame.led.set_color(0, 0, 100)")
